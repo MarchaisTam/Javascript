e4f6257eb639399ff16fc8e1514074fcfbd57c33
MarchaisTam <87361035+MarchaisTam@users.noreply.github.com>
2022-08-30 10:57:26 +0200

    ajout exo calculette

--- script.js
+++ script.js
@@ -92,7 +92,7 @@ L’utilisateur devra rentré son poids en kg, donc à vous de faire la conversi
 faire le calcul nécessaire pour l’IMC et d’afficher le résultat arrondi à 1 chiffre après la virgule.
  */
 
-  /*   let poids = prompt("Quel est votre poids en kg ?");
+    /*   let poids = prompt("Quel est votre poids en kg ?");
     let taille = prompt("Quel est votre taille en cm ?");
     poids = parseInt(poids);
     taille = parseInt(taille);
@@ -195,15 +195,115 @@ et l’affichez.
 « Switch case » vous sera utile pour cette exercice. */
 
 
-let sort = prompt("quel sort voulez vous lancer ?")
-switch (sort) {
-    case "chaine d’éclair":
-        alert("chaine d’éclair fait 25 dégats");
-        break;
-    case "boule de feu":
-        alert("boule de feu fait 40 dégats");
-        break;
-    case "trait de glace":
-        alert("trait de glace fait 35 dégats");
-        break;
+
+
+
+/*try {
+
+    let sort = prompt("quel sort voulez vous lancer ?");
+
+    switch (sort) {
+        case "chaine d’éclair":
+            alert("chaine d’éclair fait 25 dégats");
+            break;
+        case "boule de feu":
+            alert("boule de feu fait 40 dégats");
+            break;
+        case "trait de glace":
+            alert("trait de glace fait 35 dégats");
+            break;
+        default :
+            alert("Sort non reconnu");
+            throw new Error("Sort non reconnu");
+    }
+
+    
+} catch (e) {
+    console.log("for new Error()");
+    console.log(e);
+}*/
+
+
+
+/*Pour voir si tout vas bien jusque ici nous allons créer un calculateur en boite de dialogue
+(une sorte de calculatrice simplifiée).
+Objectif :
+- Demandez de faire un choix entre addition – soustraction – multiplication – division
+- Indice : pour un retour à la ligne « \n » tant que l’utilisateur ne choisis pas une des quatre
+proposition il serait bien de répéter la question.
+- Demandez de rentrer un nombre, puis un deuxieme
+- Indice : Répéter la demande si un nombre n’est pas rentré, je vous laisse chercher la
+fonction qui permet de checker cela.
+- Crée 4 fonctions correspond aux 4 méthodes de calculs
+- Selon le choix de l’utilisateur appelé la fonction correspondante
+- Affichez le résultat
+- Proposez à l’utilisateur de recommencer*/
+
+
+let operation = prompt("Choix entre addition – soustraction – multiplication – division");
+console.log(operation);
+
+
+while(operation !== "addition" && operation !== "soustraction" && operation !== "multiplication" && operation !== "division"){
+    operation = prompt("Choix entre addition – soustraction – multiplication – division");
 }
+console.log(operation);
+
+
+let a = parseInt(prompt("Choisissez un premier chiffre"));
+while(isNaN(a) && a !== " "){
+    alert("Premier chiffre incorrect");
+    a = prompt("Choisissez un premier chiffre");
+}
+console.log(a);
+
+
+
+let b = parseInt(prompt("Choisissez un deuxième chiffre"));
+while(isNaN(b) && b !== " "){
+    alert("Deuxième chiffre incorrect");
+    b = prompt("Choisissez un deuxième chiffre");
+}
+console.log(b);
+
+
+
+function addition(a, b) {
+    let res = a + b;
+    console.log("L'addition de " + a + " + " + b + " est égale à " + res);
+}
+
+function soustraction(a, b) {
+    let res = a - b;
+    console.log("La soustraction de " + a + " + " + b + " est égale à " + res);
+}
+
+function multiplication(a, b) {
+    let res = a * b;
+    console.log("La multiplication de " + a + " + " + b + " est égale à " + res);
+}
+
+function division(a, b) {
+    let res = a / b;
+    console.log("La division de " + a + " + " + b + " est égale à " + res);
+}
+
+switch (operation) {
+    case "addition":
+        addition(a,b);
+        break;
+    case "soustraction":
+        soustraction(a,b);
+        break;
+    case "multiplication":
+        multiplication(a,b);
+        break;
+    case "division":
+        division(a,b);
+        break;
+    default :
+        alert("opération non reconnu");
+}
+
+
+
